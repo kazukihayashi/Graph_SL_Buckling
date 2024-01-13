9c89831b3829e38be9638eed1b3b453d6ae72935
Kazuki Hayashi <hayashi.kazuki.55a@gmail.com>
2024-01-14 01:38:06 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,12 +4,13 @@ import Environment
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--use_gpu", help="Use GPU if True, use CPU if False", action='store_true')
+parser.add_argument("--n_train_data", help="Number of samples to train the machine learning model.", default=1000, type=int)
 parser.add_argument("--n_epoch", help="Number of epochs to train the machine learning model.", default=200, type=int)
 args = parser.parse_args()
 
 t1 = time.time()
 env = Environment.Environment(gpu=args.use_gpu)
-env.Stock() # Stock supervision data
+env.Stock(n_train_data=args.n_train_data) # Stock supervision data
 env.Train(n_epoch=args.n_epoch) # Train machine learning model
 t2 = time.time()
 print("time: {:.3f} seconds".format(t2-t1))
